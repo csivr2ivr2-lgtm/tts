96c2952a45d42959b3391c6e5ca205d68d614fad
אהרן לייב פלוינסקי <cs.ivr2ivr2@gmail.com>
2026-09-14 07:40:21 +0300

    Report v0.3.2

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ import { defaultProfilePath, buildVoiceProfile, inspectVoiceProfile, loadVoicePr
 
 for (const k of ["OMP_NUM_THREADS", "MKL_NUM_THREADS", "OPENBLAS_NUM_THREADS", "ORT_NUM_THREADS"]) process.env[k] ||= "1";
 
-const VERSION = "0.3.1";
+const VERSION = "0.3.2";
 const PORT = Number(process.env.PORT || 3000);
 const API_KEY = process.env.TTS_API_KEY || "";
 const LANGUAGE = process.env.TTS_LANGUAGE || "hebrew";
